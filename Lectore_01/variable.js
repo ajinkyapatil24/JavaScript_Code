5da9ec7138da93d8d4823d0ccc53ad9206745e6c
Ajinkya Patil <ajinkyapatil240499@gmail.com>
2024-10-30 01:30:38 +0530

    variabels explain var,let and const

---
+++ Lectore_01/variable.js
@@ -0,0 +1,17 @@
+//! var variable
+var name = "harry";
+var name = "spider";
+
+console.log(name); //spider
+
+//! let Variable
+// let name = "lenovo";
+// let name = "hp";
+
+// console.log(name); //SyntaxError
+
+//!const Variable
+// const id = "AJ24";
+// const id = "AP24";
+
+// console.log(id); //SyntaxError
